11ae93879667ea899d24a14c5b04699a7377d142
Joel Guillen <noctis2001@gmail.com>
2025-03-01 20:15:55 -0400

    style: add vertical scroll to the BookTable component

--- client/src/Components/BookTable/BookTable.tsx
+++ client/src/Components/BookTable/BookTable.tsx
@@ -11,35 +11,37 @@ export function BookTable() {
   }
 
   return (
-    <table id="book-list">
-      <thead>
-        <tr>
-          <th>Título</th>
-          <th>Género</th>
-          <th>Autor</th>
-          <th>Numero de Páginas</th>
-          <th>Número de palabras</th>
-          <th>Modificar</th>
-          <th>Eliminar</th>
-        </tr>
-      </thead>
-      <tbody>
-        {books.map((book) =>
-          <tr key={book.title}>
-            <td>{book.title}</td>
-            <td>{book.genre}</td>
-            <td>{book.author}</td>
-            <td>{book.pages}</td>
-            <td>{book.words}</td>
-            <td>{book && <button onClick={() => {
-              setBookToChangeState(book)
-              openModal("modal-2")
-            }} className="editBookButton">Modificar</button>}</td>
-            <td>{book && <button onClick={() => deleteBook(book)} className="deleteBookButton">X</button>}</td>
+    <div id="book-list-container">
+      <table id="book-list">
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Género</th>
+            <th>Autor</th>
+            <th>Numero de Páginas</th>
+            <th>Número de palabras</th>
+            <th>Modificar</th>
+            <th>Eliminar</th>
           </tr>
-        )
-        }
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {books.map((book) =>
+            <tr key={book.title}>
+              <td>{book.title}</td>
+              <td>{book.genre}</td>
+              <td>{book.author}</td>
+              <td>{book.pages}</td>
+              <td>{book.words}</td>
+              <td>{book && <button onClick={() => {
+                setBookToChangeState(book)
+                openModal("modal-2")
+              }} className="editBookButton">Modificar</button>}</td>
+              <td>{book && <button onClick={() => deleteBook(book)} className="deleteBookButton">X</button>}</td>
+            </tr>
+          )
+          }
+        </tbody>
+      </table>
+    </div>
   )
 }
